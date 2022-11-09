5243260b9e81ef2e291ee79f3d226011dd59c142
wade <wade@siign.com>
2022-11-09 16:00:03 +0800

    refactor:[第1章]1.6.1 封裝購物車計算運費的演算法
    設計模式幫你找出不明顯的抽象和描述這些抽象的物件，
    例如，**業務邏輯或演算法的物件**並不存在但它們卻是設計的關鍵部分。
    
    在購物車系統中，計算運費的邏輯是經常變動的，
    故我們在這邊進行一次重構，
    把判斷是否免運的邏輯封裝起來，變成一個「策略 Strategy」，
    供將來隨時替換計算免運的演算法。
    
    在這一次的 commit 中，
    我們在購物車裡面找出了「不明顯的抽象和描述這些抽象的物件」，
    將一行判斷邏輯「$cart->products->sum('price') >= 1000」抽象成 `運費計算策略`
    
    原本一行程式碼並不明顯足夠說明運費判斷在流程中的重要性，
    但是透過設計模式我們可以用顯示的方式來表達購物車裡面有一個很重要的「運費計算策略」
    
    重構後記得執行測試案例，確保系統的行為仍符合需求規格：
    \Tests\Feature\Ch1\ShoppingCartTest::testCalDeliveryFee

---
+++ testcase-laravel/tests/Feature/Ch1/DeliverFeeStrategyDefault.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Tests\Feature\Ch1;
+
+/**
+ * 計算是否免運的策略
+ *
+ * 這是系統最預設的免運判斷邏輯，只要滿千就免運
+ */
+class DeliverFeeStrategyDefault
+{
+
+    public function __construct()
+    {
+    }
+
+    /**
+     * 是否符合免運條件
+     * @param ShoppingCart $cart
+     * @return bool
+     */
+    public function isFree(ShoppingCart $cart)
+    {
+        return $cart->products->sum('price') >= 1000;
+    }
+}
